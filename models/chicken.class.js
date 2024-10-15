07138e7d1581c1d2c97780c8b06a79e606df2688
VeyselKaraali <veysel-karaali@outlook.de>
2024-10-16 00:03:17 +0200

    scale character and enemies

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject {
-    y= 225;
+    height = 60;
+    width = 80;
+    y = 360;
     constructor() {
         super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
         this.x = 200 + Math.random() * 500;
