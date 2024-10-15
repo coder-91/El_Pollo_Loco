01630eb50c99395076449e1222a4beedeaf294dc
VeyselKaraali <veysel-karaali@outlook.de>
2024-10-16 01:01:06 +0200

    added imageCache

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject {
     width = 100;
     height = 250;
     img;
+    imgCache = {};
 
     loadImage(path) {
         this.img = new Image()
         this.img.src = path;
+    }
+
+    loadImages(paths) {
+        paths.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imgCache[path] = path;
+        })
     }
 
     moveLeft() {
