01386307aa347f9867e79af92dd2bfcf39b6ca1c
VeyselKaraali <veysel-karaali@outlook.de>
2024-10-30 19:51:07 +0100

    Throw bottle

---
+++ models/throwable-object.class.js
@@ -0,0 +1,18 @@
+class ThrowableObject extends MovableObject {
+    constructor(x, y) {
+        super().loadImage("img/7_statusbars/3_icons/icon_salsa_bottle.png");
+        this.x = x;
+        this.y = y;
+        this.width = 75;
+        this.height = 100;
+        this.throw();
+    }
+
+    throw() {
+        this.speedY = 30;
+        this.applyGravity();
+        setInterval(() => {
+            this.x +=10;
+        }, 25);
+    }
+}
