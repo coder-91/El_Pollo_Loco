ed7c59c42595f46e8fbd39e6f2193e6bd4f698f5
VeyselKaraali <veysel-karaali@outlook.de>
2024-10-30 16:54:38 +0100

    Collision detection

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -58,6 +58,13 @@ class MovableObject {
         }
     }
 
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height;
+    }
+
     moveLeft() {}
 
     moveRight() {}
