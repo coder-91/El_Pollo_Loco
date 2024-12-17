28935bd3f33e155da4544cc608083d9ae38eedb5
VeyselKaraali <veysel-karaali@outlook.de>
2024-12-17 02:45:35 +0100

    Code Refactoring level1

--- src/js/level1.js
+++ src/js/level1.js
@@ -1,44 +1,54 @@
+const BACKGROUND_WIDTH = CANVAS.WIDTH - 1;
+const INITIAL_OFFSET = BACKGROUND_WIDTH * -1;
+const segmentCount = 5;
+
+const BACKGROUND_LAYERS = [
+    ["4_fourth/1.png", "3_third/2.png", "2_second/2.png", "1_first/2.png"],
+    ["4_fourth/1.png", "3_third/1.png", "2_second/1.png", "1_first/1.png"],
+];
+
+function createChicks(count) {
+    return Array.from({ length: count }, (_, i) => new Chick(i));
+}
+
+function createChickens(count) {
+    return Array.from({ length: count }, (_, i) => new Chicken(i));
+}
+
+function createClouds(CloudType1, CloudType2, count) {
+    return [
+        ...Array.from({ length: count }, () => new CloudType1()),
+        ...Array.from({ length: count }, () => new CloudType2()),
+    ];
+}
+
+function createBackgroundObjects(initialOffset, width, layersPerSegment, segmentCount) {
+    const backgroundObjects = [];
+    for (let segment = 0; segment < segmentCount; segment++) {
+        const offset = initialOffset + segment * width;
+        const layers = layersPerSegment[segment % layersPerSegment.length];
+        layers.forEach(layerPath => {
+            backgroundObjects.push(new BackgroundObject(offset, `assets/img/3_background/layers/${layerPath}`));
+        });
+    }
+    return backgroundObjects;
+}
+
+function createBottles(count) {
+    return Array.from({ length: count }, (_, i) => new Bottle(i));
+}
+
+function createCoins(count) {
+    return Array.from({ length: count }, (_, i) => new Coin(i));
+}
+
 const level1 = new Level(
     [
-        ...Array(6).fill().map((_, i) => new Chick(i)),
-
-        ...Array(6).fill().map((_, i) => new Chicken(i))
+        ...createChicks(6),
+        ...createChickens(6)
     ],
-    [
-        ...Array.from({ length: 5 }, () => new Cloud1()),
-        ...Array.from({ length: 5 }, () => new Cloud2())
-
-    ],
-    [
-        new BackgroundObject(-719, "assets/img/3_background/layers/4_fourth/1.png"),
-        new BackgroundObject(-719, "assets/img/3_background/layers/3_third/2.png"),
-        new BackgroundObject(-719, "assets/img/3_background/layers/2_second/2.png"),
-        new BackgroundObject(-719, "assets/img/3_background/layers/1_first/2.png"),
-
-        new BackgroundObject(0, "assets/img/3_background/layers/4_fourth/1.png"),
-        new BackgroundObject(0, "assets/img/3_background/layers/3_third/1.png"),
-        new BackgroundObject(0, "assets/img/3_background/layers/2_second/1.png"),
-        new BackgroundObject(0, "assets/img/3_background/layers/1_first/1.png"),
-
-        new BackgroundObject(719, "assets/img/3_background/layers/4_fourth/1.png"),
-        new BackgroundObject(719, "assets/img/3_background/layers/3_third/2.png"),
-        new BackgroundObject(719, "assets/img/3_background/layers/2_second/2.png"),
-        new BackgroundObject(719, "assets/img/3_background/layers/1_first/2.png"),
-
-        new BackgroundObject(719*2, "assets/img/3_background/layers/4_fourth/1.png"),
-        new BackgroundObject(719*2, "assets/img/3_background/layers/3_third/1.png"),
-        new BackgroundObject(719*2, "assets/img/3_background/layers/2_second/1.png"),
-        new BackgroundObject(719*2, "assets/img/3_background/layers/1_first/1.png"),
-
-        new BackgroundObject(719*3, "assets/img/3_background/layers/4_fourth/1.png"),
-        new BackgroundObject(719*3, "assets/img/3_background/layers/3_third/2.png"),
-        new BackgroundObject(719*3, "assets/img/3_background/layers/2_second/2.png"),
-        new BackgroundObject(719*3, "assets/img/3_background/layers/1_first/2.png"),
-    ],
-    [
-        ...Array(6).fill().map((_, i) => new Bottle(i)),
-    ],
-    [
-        ...Array(5).fill().map((_, i) => new Coin(i)),
-    ]
-)
+    createClouds(Cloud1, Cloud2, 5),
+    createBackgroundObjects(INITIAL_OFFSET, BACKGROUND_WIDTH, BACKGROUND_LAYERS, segmentCount),
+    createBottles(6),
+    createCoins(5)
+);
