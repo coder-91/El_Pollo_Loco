70cbbf28a95030372752536d70744338b91a48c8
VeyselKaraali <veysel-karaali@outlook.de>
2025-01-22 13:21:38 +0100

    Changed value of WIDTH_MAX_X

--- src/js/models/configs/world-config.class.js
+++ src/js/models/configs/world-config.class.js
@@ -1,10 +1,10 @@
 class WorldConfig {
+    static SEGMENT_COUNT = 6;
     static WIDTH= 720;
     static HEIGHT = 480;
-    static WIDTH_MAX_X = 2200;
+    static WIDTH_MAX_X = 2500;
     static BACKGROUND_WIDTH = WorldConfig.WIDTH - 1;
     static INITIAL_OFFSET = WorldConfig.BACKGROUND_WIDTH * -1;
-    static SEGMENT_COUNT = 5;
     static BACKGROUND_LAYERS = [
         ["4_fourth/1.png", "3_third/2.png", "2_second/2.png", "1_first/2.png"],
         ["4_fourth/1.png", "3_third/1.png", "2_second/1.png", "1_first/1.png"],
